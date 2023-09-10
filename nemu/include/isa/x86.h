a6d13a269bd3936d294070c8228359bfd03e6183
tracer-ics2020 <tracer@njuics.org>
2023-09-10 14:26:26 +0800

    >  compile 211220163  Static hostname: debian        Icon name: computer-vm          Chassis: vm 🖴       Machine ID: 91cd0e37e3c84730bab040537c99ebaa          Boot ID: d25a3112752047438fdf29a95b86b6e2   Virtualization: vmware Operating System: Debian GNU/Linux 12 (bookworm)           Kernel: Linux 6.1.0-10-amd64     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform Firmware Version: 6.00  14:26:26 up  1:01,  1 user,  load average: 0.22, 0.20, 0.18

--- nemu/include/isa/x86.h
+++ nemu/include/isa/x86.h
@@ -18,6 +18,7 @@
  */
 
 typedef struct {
+	union{
   union {
 	uint32_t _32;
 	uint16_t _16;
@@ -30,7 +31,7 @@ typedef struct {
    * in PA2 able to directly access these registers.
    */
   rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
+	};
   vaddr_t pc;
 } x86_CPU_state;
 
