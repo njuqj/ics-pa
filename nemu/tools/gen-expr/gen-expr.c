93c475638e3274ced06b4241c5bc16eff7172ae6
tracer-ics2020 <tracer@njuics.org>
2023-09-16 13:35:06 +0800

    >  compile 211220163  Static hostname: debian        Icon name: computer-vm          Chassis: vm 🖴       Machine ID: 91cd0e37e3c84730bab040537c99ebaa          Boot ID: a392c8f504b64cec8b2b2eaa44b41adc   Virtualization: vmware Operating System: Debian GNU/Linux 12 (bookworm)           Kernel: Linux 6.1.0-10-amd64     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform Firmware Version: 6.00  13:35:06 up  5:03,  1 user,  load average: 0.18, 0.17, 0.18

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -9,27 +9,77 @@
 static char buf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
-"#include <stdio.h>\n"
-"int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
-"  return 0; "
-"}";
-
-static inline void gen_rand_expr() {
-  buf[0] = '\0';
+    "#include <stdio.h>\n"
+    "int main() { "
+    "  unsigned result = %s; "
+    "  printf(\"%%u\", result); "
+    "  return 0; "
+    "}";
+void gen_num()
+{
+  int num = rand() % 10000;
+  char str[6];
+  sprintf(str, "%d", num);
+  strcat(buf, str);
+  return;
+}
+void gen_rand_op()
+{
+  switch (rand() % 4)
+  {
+  case 0:
+    strcat(buf, "+");
+    break;
+  case 1:
+    strcat(buf, "-");
+    break;
+  case 2:
+    strcat(buf, "*");
+    break;
+  default:
+    strcat(buf, "/");
+    break;
+  }
 }
 
-int main(int argc, char *argv[]) {
+static inline void gen_rand_expr()
+{
+  switch (rand() % 3)
+  {
+  case 0:
+    gen_num();
+    break;
+  case 1:
+    strcat(buf, "(");
+    gen_rand_expr();
+    strcat(buf, ")");
+    break;
+  default:
+    gen_rand_expr();
+    gen_rand_op();
+    gen_rand_expr();
+    break;
+  }
+}
+
+int main(int argc, char *argv[])
+{
   int seed = time(0);
   srand(seed);
   int loop = 1;
-  if (argc > 1) {
+  if (argc > 1)
+  {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
-  for (i = 0; i < loop; i ++) {
+  for (i = 0; i < loop; i++)
+  {
     gen_rand_expr();
+    if (buf[60000] > 0)
+    {
+      memset(buf, 0, sizeof(buf));
+      continue;
+    }
 
     sprintf(code_buf, code_format, buf);
 
@@ -39,7 +89,8 @@ int main(int argc, char *argv[]) {
     fclose(fp);
 
     int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+    if (ret != 0)
+      continue;
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
@@ -49,6 +100,7 @@ int main(int argc, char *argv[]) {
     pclose(fp);
 
     printf("%u %s\n", result, buf);
+    memset(buf, 0, sizeof(buf));
   }
   return 0;
 }
