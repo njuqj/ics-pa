f5dcf6a6e1e4cac3b3f73ba0fdfefecc07edc975
tracer-ics2020 <tracer@njuics.org>
2023-09-18 14:56:05 +0800

    >  compile 211220163  Static hostname: debian        Icon name: computer-vm          Chassis: vm 🖴       Machine ID: 91cd0e37e3c84730bab040537c99ebaa          Boot ID: dbdc9bc275c64ffcb0ffa3c41f7d40f9   Virtualization: vmware Operating System: Debian GNU/Linux 12 (bookworm)           Kernel: Linux 6.1.0-10-amd64     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform Firmware Version: 6.00  14:56:05 up  1:06,  1 user,  load average: 0.25, 0.21, 0.15

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -169,7 +169,7 @@ static bool check_parenthess(int p, int q)
   }
 }
 
-static uint32_t eval(int p, int q)
+uint32_t eval(int p, int q)
 {
   if (p > q)
   {
@@ -288,6 +288,6 @@ word_t expr(char *e, bool *success)
   // printf("%d\n", res);
   /* TODO: Insert codes to evaluate the expression. */
   printf("%u\n", eval(0, nr_token - 1));
-  memset(tokens, 0, sizeof(tokens));
+  // memset(tokens, 0, sizeof(tokens));
   return 0;
 }
