feb056825523e09b613132bdabf95be44975d030
tracer-ics2020 <tracer@njuics.org>
2023-09-22 14:27:58 +0800

    >  compile 211220163  Static hostname: debian        Icon name: computer-vm          Chassis: vm 🖴       Machine ID: 91cd0e37e3c84730bab040537c99ebaa          Boot ID: 7e08c91a027e48c18f4729c8930234b9   Virtualization: vmware Operating System: Debian GNU/Linux 12 (bookworm)           Kernel: Linux 6.1.0-10-amd64     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform Firmware Version: 6.00  14:27:58 up  1:41,  1 user,  load average: 0.15, 0.13, 0.09

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void init_monitor(int, char *[]);
 void engine_start();
 int is_exit_status_bad();
@@ -6,6 +8,14 @@ int main(int argc, char *argv[])
 {
   /* Initialize the monitor. */
   init_monitor(argc, argv);
+  FILE *fp = fopen("../tools/gen-expr/input", "r");
+  if (fp == NULL)
+    printf("无法打开文件。\n");
+  else
+  {
+    printf("打开！\n");
+    fclose(fp);
+  }
 
   /* Start engine. */
   engine_start();
