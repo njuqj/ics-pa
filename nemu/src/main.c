ae699852b26fbf74af7487f55744f0d3e717878a
tracer-ics2020 <tracer@njuics.org>
2023-09-22 14:48:32 +0800

    >  compile 211220163  Static hostname: debian        Icon name: computer-vm          Chassis: vm 🖴       Machine ID: 91cd0e37e3c84730bab040537c99ebaa          Boot ID: 7e08c91a027e48c18f4729c8930234b9   Virtualization: vmware Operating System: Debian GNU/Linux 12 (bookworm)           Kernel: Linux 6.1.0-10-amd64     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform Firmware Version: 6.00  14:48:32 up  2:02,  1 user,  load average: 0.11, 0.23, 0.18

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 void init_monitor(int, char *[]);
 void engine_start();
@@ -9,25 +10,9 @@ int main(int argc, char *argv[])
 {
   /* Initialize the monitor. */
   init_monitor(argc, argv);
-  FILE *fp = fopen("/home/miracle/ics2020/nemu/tools/gen-expr/input", "r");
-  if (fp == NULL)
-    printf("无法打开文件。\n");
-  else
-  {
-    char str[60030];
-    char e[60000];
-    uint32_t res = 0;
-    while (!feof(fp))
-    {
-      fgets(str, 60030, fp);
-      sscanf(str, "%u %s", &res, e);
-      printf("%u %s\n", res, e);
-    }
-    fclose(fp);
-  }
 
   /* Start engine. */
-  // engine_start();
+  engine_start();
 
   return is_exit_status_bad();
 }
